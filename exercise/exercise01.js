b6cb2161c2ea8899020d5bdca57d5cbded19390d
troie <troie@maolah.com>
2024-07-09 01:06:38 +0800

    update

--- exercise/exercise01.js
+++ exercise/exercise01.js
@@ -13,3 +13,17 @@ console.log("變數 b 等於 " + b);
 
 //變數 a 等於 3
 //變數 b 等於 7
+
+var user = ["troie","bob","carie","dreak","frank"];
+user.forEach(v,i,e){console.log(v i e);}
+
+newArray[0] = 1;
+newArray[1] = 2;
+newArray[2] = 3;
+newArray[3] = 4;
+newArray[4] = 5;
+newArray[5] = 6;
+newArray[6] = 7;
+newArray[7] = 8;
+newArray[8] = 9;
+newArray[9] = 10;
--- js/guest_number_game.js
+++ js/guest_number_game.js
@@ -1,39 +1,20 @@
-function getRandom() {
-  return parseInt(Math.random()*100);
-}
-
-function guessNumber(num){
-    if(num > ansNumber) console.log("太大");
-    else if(num < ansNumber) console.log("太小");
+function getRandom(scope) {
+  return parseInt(Math.random()*scope);
 }
 
 function gameStart(){
+    var ansNumber = getRandom(100);
+    console.log(ansNumber);
+    var msg="0 ~ 100" ;
     while(true){
-        num = prompt("guest a number?");
-        if(num == ansNumber){
+        num = prompt("guest a number ("+msg+")?");
+        if(num == ansNumber || num == 0){
+            alert("歡呼聲！！！");
             break;
         }else{
-            guessNumber(num);
+            if(num > ansNumber) msg = "猜小一點";
+            else if(num < ansNumber) msg = "猜大一點";
         }
     }
 }
-var ansNumber = getRandom();
-gameStart();
 
-var transportation = prompt("你將使用哪一種交通工具 ?");
-switch(n){
-    case "走路":
-        console.log("大約需要30分鐘");
-        break;
-    case "機車":
-        console.log("大約需要10分鐘");
-        break;
-    case "公車":
-        console.log("大約需要15分鐘");
-        break;
-    case "捷運":
-        console.log("大約需要20分鐘");
-        break;
-    defaule:
-        console.log("無法估計時間");
-}
